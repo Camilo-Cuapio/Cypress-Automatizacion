d2dac4d170e0f49bee8a7c1c665214a05fac148e
Camilo-Cuapio <cuapio.camilo@gmail.com>
2026-02-05 18:12:33 -0600

    Actualiza test.cy.js

--- cypress/e2e/1-getting-started/test.cy.js
+++ cypress/e2e/1-getting-started/test.cy.js
@@ -6,6 +6,7 @@ it("logeo usuario y contraseña",()=>{
     cy.get('[data-test="username"]').type("standard_user")
     cy.get('[data-test="password"]').type("secret_sauce")
     cy.get('[data-test="login-button"]').click()
-    
+  cy.get('.app_logo').should("be.visible")
+
 })
 })
