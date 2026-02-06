392fe1f75df877179fc35fc47af15ef1cff27842
Camilo-Cuapio <cuapio.camilo@gmail.com>
2026-02-05 18:29:33 -0600

    Actualiza test.cy.js

--- cypress/e2e/1-getting-started/test.cy.js
+++ cypress/e2e/1-getting-started/test.cy.js
@@ -7,6 +7,15 @@ it("logeo usuario y contraseña",()=>{
     cy.get('[data-test="password"]').type("secret_sauce")
     cy.get('[data-test="login-button"]').click()
   cy.get('.app_logo').should("be.visible")
+})
 
+it("Logeo con usuario y contraseña incorrecta",()=>{
+    cy.get('[data-test="username"]').type("standard_user")
+    cy.get('[data-test="password"]').type("Error")
+    cy.get('[data-test="login-button"]').click()
+cy.get('[data-test="error"]').should("be.visible")
+cy.get('[data-test="error"]').should("be.visible")
+cy.get('[data-test="error"]').should("be.visible")
+cy.get('[data-test="error-button"]').should("be.visible")
 })
 })
